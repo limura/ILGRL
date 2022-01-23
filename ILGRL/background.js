90d6375b2fc3a80cd49033354f1c2cca3e34db80
IIMURA Takuji <uirou.j@gmail.com>
2022-01-23 15:50:54 +0900

    Manifest V3 対応

--- ILGRL/background.js
+++ ILGRL/background.js
@@ -10,14 +10,16 @@ var linkText = undefined;
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.contextMenus.create({
 		"type": "normal"
-		, "title": chrome.i18n.getMessage("ContextMenuText_NormalSearch")
+		// TODO: chrome.i18n.getMessage が service worker でも動くようになったらこのコメントを外して多国語化する。
+		, "title": "リンクの文字列を検索"//chrome.i18n.getMessage("ContextMenuText_NormalSearch")
 		, "id": NORMAL_SEARCH
 		, "contexts": ["link"]
 		, "documentUrlPatterns": ["http://*/*", "https://*/*"]
 	});
 	chrome.contextMenus.create({
 		"type": "normal"
-		, "title": chrome.i18n.getMessage("ContextMenuText_ImFeelingLuckySearch")
+		// TODO: chrome.i18n.getMessage が service worker でも動くようになったらこのコメントを外して多国語化する。
+		, "title": "リンクの文字列を検索 (I'm Feeling Lucky)"//chrome.i18n.getMessage("ContextMenuText_ImFeelingLuckySearch")
 		, "id": IM_FEELING_LUCKEY
 		, "contexts": ["link"]
 		, "documentUrlPatterns": ["http://*/*", "https://*/*"]
@@ -28,14 +30,19 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.contextMenus.onClicked.addListener(function(info, tab){
 	//console.log("context menu clicked", info, tab, linkText);
 	if(info.menuItemId == IM_FEELING_LUCKEY){
-		window.open(IM_FEELING_LUCKY_QUERY + linkText);
+		chrome.tabs.create({
+			openerTabId: tab.id,
+			url: IM_FEELING_LUCKY_QUERY + linkText
+		});
 	}else{
-		window.open(SEARCH_QUERY + linkText);
+		chrome.tabs.create({
+			openerTabId: tab.id,
+			url: SEARCH_QUERY + linkText
+		});
 	}
 });
 
 // add message handler for content script
-chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 	linkText = msg;
 });
-
--- ILGRL/content_script.js
+++ ILGRL/content_script.js
@@ -3,7 +3,7 @@ document.addEventListener( "contextmenu", function(event) {
     var limit = 5;
     while ( node && ( limit-- > 0 ) ) {
         if ( node.nodeName === "A" && node.href != undefined ) {
-            chrome.extension.sendMessage(node.innerText);
+            chrome.runtime.sendMessage(node.innerText);
             return true;
         } else {
             node = node.parentNode;
